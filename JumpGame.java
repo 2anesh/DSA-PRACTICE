24f9ef78328c0e05377fb15210525eba14b39138
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2022-12-29 17:17:13 +0530

    Create JumpGame.java

---
+++ JumpGame.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public boolean canJump(int[] nums) {
+
+        int n=nums.length,i=n-2;
+
+        int goal=n-1;
+
+        while(i>=0){
+
+            if(i+nums[i]>=goal){
+
+                goal=i;
+
+                i--;
+
+            }else{i--;}
+
+        }
+
+        return goal==0?true:false;
+
+    }
+
+}
