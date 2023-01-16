8e16574b1dc3e5c412fe95ecf4fc203b73eb0c8b
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2023-01-16 17:56:20 +0530

    Create Next Greater Element.cpp

---
+++ Next Greater Element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    vector<long long> nextLargerElement(vector<long long> &arr, int n){
+        vector<long long> ans(n, -1);
+        stack<long long> st;
+        st.push(arr[n-1]);
+        for(int i=n-2; i>=0; i--){
+            while(!st.empty() && arr[i]>=st.top()){
+                st.pop();
+            }   
+            if(!st.empty()){
+                ans[i]=st.top();
+            }
+            st.push(arr[i]);
+        }
+        return ans;
+    }
+};
