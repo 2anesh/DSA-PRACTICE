b6b85c63d1759aff72ef05a719ce4154540ba7b1
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2023-01-26 13:13:38 +0530

    Create Case-specific Sorting of Strings.cpp

---
+++ Case-specific Sorting of Strings.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+    public:
+    //Function to perform case-specific sorting of strings.
+    string caseSort(string s, int n){
+        string lower="";
+        string upper="";
+        
+        for(int i=0; i<n; i++){
+            if(isupper(s[i])) upper += s[i];
+            else lower += s[i];
+        }
+        
+        sort(lower.begin(), lower.end());
+        sort(upper.begin(), upper.end());
+        
+        int idx_upper=0;
+        int idx_lower=0;
+        
+        for(int i=0; i<n; i++){
+            if(idx_upper < upper.size()){
+                if(isupper(s[i])){
+                    s[i] = upper[idx_upper];
+                    idx_upper++;
+                }
+            }
+            if(idx_lower < lower.size()){
+                if(islower(s[i])){
+                    s[i] = lower[idx_lower];
+                    idx_lower++;
+                }
+            }
+        }
+        return s;
+    }
+};
