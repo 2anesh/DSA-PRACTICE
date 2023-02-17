d0708c7f30bd3fd5b5a1ac664d3c8ba85c0bad66
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2023-02-17 07:42:22 +0530

    Create  Minimum Distance Between BST Nodes.java

---
+++ Minimum Distance Between BST Nodes.java
@@ -0,0 +1,18 @@
+class Solution {
+int min = Integer.MAX_VALUE;
+Integer prev = null;
+public int minDiffInBST(TreeNode root)
+{
+if(root == null) return min;
+minDiffInBST(root.left);
+if(prev != null)
+{
+min = Math.min(min, root.val-prev);
+}
+
+    prev = root.val;
+    minDiffInBST(root.right);
+    
+    return min;
+}
+}
