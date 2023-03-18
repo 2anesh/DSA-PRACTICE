ef1eba94a8ece936e1f8f322b22a70bab511d947
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2023-03-18 22:52:20 +0530

    Create

---
+++ Median of the Subarrays.java
@@ -0,0 +1,26 @@
+class Solution {
+    long solve(int n, int[] A, int m) {
+        int[] mp = new int[2 * n + 1];
+        long cur = n, tot = 0, ans = 0;
+        mp[(int) cur]++;
+        for (int i = 0; i < n; i++) {
+            int x = -1;
+            if (A[i] >= m) {
+                x = 1;
+            }
+            if (x == -1) {
+                tot -= mp[(int) (cur + x)];
+            } else {
+                tot += mp[(int) cur];
+            }
+            cur += x;
+            ans += tot;
+            mp[(int) cur]++;
+        }
+        return ans;
+    }
+
+    long countSubarray(int N, int[] A, int M) {
+        return solve(N, A, M) - solve(N, A, M + 1);
+    }
+}
