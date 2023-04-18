aa1586323af66f1e52d0157b862c066eedd53256
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2023-04-18 23:08:59 +0530

    Create  Merge Strings Alternately.cpp

---
+++ Merge Strings Alternately.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i = 0, j = 0;
+        string merged = "";
+        while (i < word1.length() && j < word2.length()) {
+            merged += word1[i++];
+            merged += word2[j++];
+        }
+        while (i < word1.length()) {
+            merged += word1[i++];
+        }
+        while (j < word2.length()) {
+            merged += word2[j++];
+        }
+        return merged;
+    }
+};
