f14755ab75df8411f6c753eafa34ebacdaf99950
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2023-08-10 18:40:21 +0530

    Create Longest Common Subsequence.cpp

---
+++ Longest Common Subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+    public:
+    //Function to find the length of longest common subsequence in two strings.
+    int lcs(int n, int m, string s1, string s2)
+    {
+        int kp[n+1][m+1];
+        for(int i =0; i<=n; i++){
+            for(int j = 0; j<=m; j++){
+                if(i==0||j==0){
+                    kp[i][j]=0;
+                }
+            }
+        }
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
+                if(s1[i-1] == s2[j-1]){
+                    kp[i][j] = 1+kp[i-1][j-1];
+                }
+                else {
+                    kp[i][j] = max(kp[i-1][j],kp[i][j-1]);
+                }
+            }
+        }
+        return kp[n][m];
+    }
+};
