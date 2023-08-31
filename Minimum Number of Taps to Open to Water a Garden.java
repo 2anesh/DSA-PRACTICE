91e3b4655b90f840e76d1cc4072eb02733af7811
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2023-08-31 14:36:18 +0530

    Create  Minimum Number of Taps to Open to Water a Garden.java

---
+++ Minimum Number of Taps to Open to Water a Garden.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minTaps(int n, int[] ranges) {
+        int min = 0;
+        int max = 0;
+        int open = 0;
+        int index =0;
+        while(max < n){
+            for(int i = index ; i < ranges.length ; i++){
+                if(i-ranges[i]<= min && i+ranges[i]>max){
+                    max = i+ranges[i];
+                }
+            }
+            if(min == max) return -1;
+            open++;
+            min = max;
+        }
+        return open;
+    }
+}
