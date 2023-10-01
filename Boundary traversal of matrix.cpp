655ecb1eba29bd29dc4f9b440740ae356e71b05d
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2023-10-01 20:39:01 +0530

    Create Boundary traversal of matrix.cpp

---
+++ Boundary traversal of matrix.cpp
@@ -0,0 +1,40 @@
+class Solution
+{   
+    public:
+    //Function to return list of integers that form the boundary 
+    //traversal of the matrix in a clockwise manner.
+    vector<int> boundaryTraversal(vector<vector<int> > matrix, int n, int m) 
+    {
+        vector<int> ans;
+        
+        //only 1 row
+        if(n == 1) {
+            for(int i =0 ; i < m; i++) {
+                ans.push_back(matrix[0][i]);
+            }
+            return ans;
+        }
+        
+        //only 1 col
+        if(m == 1) {
+            for(int i =0 ; i < n; i++) {
+                ans.push_back(matrix[i][0]);
+            }
+            return ans;
+        }
+        
+        for(int i =0 ; i < m-1 ; i++) {
+                ans.push_back(matrix[0][i]);
+        }
+        for(int i =0 ; i < n-1 ; i++) {
+                ans.push_back(matrix[i][m-1]);
+        }
+        for(int i =m -1 ; i >= 1 ; i--) {
+                ans.push_back(matrix[n-1][i]);
+        }
+        for(int i =n -1 ; i >= 1 ; i--) {
+                ans.push_back(matrix[i][0]);
+        }
+        return ans;
+    }
+};
