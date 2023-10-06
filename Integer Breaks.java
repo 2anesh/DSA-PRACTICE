b7f3fc894bf95549327d455afce18d359b14e706
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2023-10-06 23:08:21 +0530

    Create Integer Breaks.java

---
+++ Integer Breaks.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int integerBreak(int n) {
+       if (n == 2) {
+            return 1;
+        } 
+        else if (n == 3) {
+            return 2;
+        } 
+        else if (n % 3 == 0) {
+            return (int) Math.pow(3, n / 3);
+        } 
+        else if (n % 3 == 1) {
+            return (int) (Math.pow(3, (n / 3) - 1) * 4);
+        } 
+        else {
+            return (int) (Math.pow(3, n / 3) * 2);
+        }
+    }
+}
