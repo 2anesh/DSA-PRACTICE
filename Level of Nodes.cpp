f1d5ba68dd88e9837fbd9bbab121edfbd6f7e510
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2023-10-19 18:11:14 +0530

    Create Level of Nodes.cpp

---
+++ Level of Nodes.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+	public:
+	int dfs(int node , int lev , vector<int> &vis , vector<int> adj[] , int X){
+	    if(node == X) return lev;
+	    vis[node] = 1;
+	    
+	    for(auto it : adj[node]){
+	        if(!vis[it]) {
+	            int level = dfs(it , lev+1 , vis , adj , X);
+	            if(level != -1) return level;
+	        }
+	    }
+	    
+	    return -1;
+	}
+	//Function to find the level of node X.
+	int nodeLevel(int V, vector<int> adj[], int X) 
+	{
+	    // code here
+	    vector<int> vis(V , 0);
+	    return dfs(0 , 0 , vis , adj , X);
+	}
+};
