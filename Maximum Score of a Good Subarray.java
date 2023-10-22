73e3c03d27908354823a33c7756acdd9f79ff972
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2023-10-22 22:31:15 +0530

    Create Maximum Score of a Good Subarray.java

---
+++ Maximum Score of a Good Subarray.java
@@ -0,0 +1,19 @@
+class Solution {
+
+public int maximumScore(int[] nums, int k) {
+int ans = 0;
+Deque stack = new ArrayDeque<>();
+
+for (int i = 0; i <= nums.length; ++i) {
+  while (!stack.isEmpty() && (i == nums.length || nums[stack.peek()] > nums[i])) {
+    final int h = nums[stack.pop()];
+    final int w = stack.isEmpty() ? i : i - stack.peek() - 1;
+    if ((stack.isEmpty() || stack.peek() + 1 <= k) && i - 1 >= k)
+      ans = Math.max(ans, h * w);
+  }
+  stack.push(i);
+}
+
+return ans;
+}
+}
