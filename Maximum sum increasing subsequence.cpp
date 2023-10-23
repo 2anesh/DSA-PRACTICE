2fb8953c58edab86c3965db899f57a75538b055f
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2023-10-23 22:44:04 +0530

    Create Maximum sum increasing subsequence.cpp

---
+++ Maximum sum increasing subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution{
+    public:
+    int maxSumIS(int arr[], int n)  
+    {  
+        // Your code goes here
+        vector<int> sum(n,0);
+        for(int i=0;i<n;i++){
+            for(int j=i-1;j>=0;j--){
+                if(arr[i]>arr[j]){
+                    sum[i]=max(sum[i],sum[j]);
+                }
+            }
+            sum[i]+=arr[i];
+        }
+        return *max_element(sum.begin(),sum.end());
+    }  
+};
