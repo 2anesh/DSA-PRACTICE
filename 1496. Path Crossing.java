747f5f1d6f558a97076b551f836fe109e7d7ec8e
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2023-12-23 08:33:13 +0530

    Create 1496. Path Crossing.java

---
+++ 1496. Path Crossing.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPathCrossing(String dir) {
+        Set<String> unqPoints = new HashSet<>();
+        char path[] = dir.toCharArray();
+        int xCord = 0, yCord = 0, sz = path.length;
+        String orgKey = Integer.toString(xCord) + " " +  Integer.toString(yCord);
+        unqPoints.add(orgKey);
+        for(int indx = 0; indx < sz; indx++){
+            if(path[indx] == 'N'){
+                yCord++;
+            }else if(path[indx] == 'S'){
+                yCord--;
+            }else if(path[indx] == 'E'){
+                xCord++;      
+            }else{
+                xCord--;
+            }
+            // cout<<xCord<<" "<<yCord<<endl;
+            String cordKey = Integer.toString(xCord) + " " +  Integer.toString(yCord);
+            if(unqPoints.contains(cordKey))return true;
+            unqPoints.add(cordKey); 
+        }    
+        return false;
+    }
+}
