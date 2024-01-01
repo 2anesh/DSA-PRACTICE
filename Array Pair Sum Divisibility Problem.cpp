c62c8cd334a87c0af1942f2c418b91231241a005
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-01-01 15:45:29 +0530

    Create Array Pair Sum Divisibility Problem.cpp

---
+++ Array Pair Sum Divisibility Problem.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    bool canPair(vector<int> nums, int k) {
+         int n=nums.size();
+         if(n%2) return false;
+         unordered_map<int,int>mp;
+         for(int i=0;i<n;i++){
+             mp[nums[i]%k]++;
+         }
+         for(auto it:mp){
+             int p=it.first;
+             int q=it.second;
+             int req=k-p;
+             if(p==0 and mp[p]%2==0) continue;
+             if(mp[req]!=q) return 0;             
+         }
+         return 1;
+    }
+};
