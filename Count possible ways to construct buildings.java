cdde4fe98c35a7891b289c294f0ff2c95cd025de
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-01-05 15:33:31 +0530

    Create Count possible ways to construct buildings.java

---
+++ Count possible ways to construct buildings.java
@@ -0,0 +1,15 @@
+class Solution{
+    public int TotalWays(int N)
+    {
+        // Code here
+        int mod = 1000000007;
+        long[] dp = new long[N+1];
+        
+        dp[0] = 1;
+        dp[1] = 2;
+        
+        for(int i=2;i<=N;i++)   dp[i] = (dp[i-1] + dp[i-2])%mod;
+        
+        return (int) ((dp[N]*dp[N])%mod);
+    }
+}
